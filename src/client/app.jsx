5f743417c295238c6b01fe8d3920c385211540a5
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-26 13:44:04 -0400

    Doing react tutorial

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store';
-import Layer from './components/layer.jsx';
 import Triggers from './components/triggers.jsx';
 
 let reactElement = document.getElementById('app');
@@ -13,3 +12,21 @@ render(
   </Provider>,
   reactElement
 );
+
+var HelloWorld = React.createClass({
+  render: function() {
+    return (
+      <p>
+        Sup, <input type="text" placeholder="Your name here" />!
+        It is {this.props.date.toTimeString()}
+      </p>
+    )
+  }
+	});
+
+setInterval(function() {
+  render(
+    <HelloWorld date={new Date()} />, 
+    document.getElementById('example')
+  );
+}, 500);
