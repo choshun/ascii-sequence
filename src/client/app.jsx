b1f1e3570e3d8f0a6e9a7722e79dcd85fcd2fec2
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-26 21:07:32 -0400

    example cleanup

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Provider} from 'react-redux';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import store from './store';
-import Triggers from './components/triggers.jsx';
+import Grid from './components/Grid.jsx';
 
 let reactElement = document.getElementById('app');
 
 render(
   <Provider store={store}>
-    <Triggers />
+    <Grid />
   </Provider>,
   reactElement
 );
-
-var HelloWorld = React.createClass({
-  render: function() {
-    return (
-      <p>
-        Sup, <input type="text" placeholder="Your name here" />!
-        It is {this.props.date.toTimeString()}
-      </p>
-    )
-  }
-	});
-
-setInterval(function() {
-  render(
-    <HelloWorld date={new Date()} />, 
-    document.getElementById('example')
-  );
-}, 500);
