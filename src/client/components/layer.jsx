c3f166df59889443856513834e4e3521f229920d
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-29 15:42:29 -0400

    got layer to pass unique css to each layer

--- src/client/components/layer.jsx
+++ src/client/components/layer.jsx
@@ -3,12 +3,12 @@ import Event from './Event.jsx';
 import { connect } from 'react-redux';
 import { addLayer } from '../actions';
 
-const Layer = ({ onClick, data }) => (
+const Layer = ({ onClick, data, css }) => (
   <li>
     <ul>
       {
-        data.map(function(event, index) {
-          return <Event key={index} data={event} />;
+        data.map((event, index) => {
+          return <Event key={event.key} data={event} css={css[event.key]} />;
         })
       }
     </ul>
