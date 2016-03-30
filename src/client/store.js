541d1f10070ca200cc3c65f49b1e4a3fb2f581f8
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-30 13:13:41 -0400

    adding style manager to store

--- src/client/store.js
+++ src/client/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore } from 'redux';
 import layers from './reducers/layers';
 import events from './reducers/events';
+import styleManager from './reducers/style-manager';
 
 let reducers = combineReducers({
   layers,
-  events
+  events,
+  styleManager
 });
 
 export default createStore(reducers);
