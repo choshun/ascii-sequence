d51255e7bfafde41051712eba2b79f67381238db
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-30 13:36:59 -0400

    adding style-manager reducer, select active style

---
+++ src/client/reducers/style-manager.js
@@ -0,0 +1,20 @@
+import Immutable from 'immutable';
+
+let selectedState = {
+  active: {
+    callback: "addStyle",
+    data: ".layer2 { blob: of css};",
+    key: "event-00-75",
+    layer: 0,
+    time: 0.75
+  }
+}
+
+export default (state = Immutable.Map(selectedState), action) => {
+  switch(action.type) {
+    case 'selectStyle':
+      return state.set('active', action.eventKey);
+    default:
+      return state;
+  }
+}
