043d551d60419b507f08590abff7dc88b3b5b9d8
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-30 21:22:26 -0400

    adding active class to event

--- src/client/components/Event.jsx
+++ src/client/components/Event.jsx
@@ -1,6 +1,8 @@
 require('./event.scss');
 
 import React from 'react';
+
+import classNames from 'classnames';
 import { deleteEvent, selectStyle } from '../actions';
 import { connect } from 'react-redux';
 
@@ -12,13 +14,21 @@ function addStyleToHead(className, css) {
   styleBlock.innerHTML = styleBlock.innerHTML + `.${className} ${css}\n`;
 }
 
+function getClassNames(eventKey, activeKey) {
+  let classObject = {};
+  classObject[eventKey] = true;
+  classObject['is-active'] = (eventKey === activeKey) ? true : false;
+
+  return classNames(classObject);
+}
+
 // TODO: add classNames to project for active stuff
 // https://github.com/JedWatson/classnames
 
 // TODO: pass in styleManager.active
 // and do classname={if (data.key === styleManager.active) then addCLass active }
-const Event = ({data, css, dispatch}) => (
-  <li className={data.key} onClick={(event) => {
+const Event = ({store, data, css, dispatch}) => (
+  <li className={getClassNames(data.key, store.styleManager.get('active').key)} onClick={(event) => {
       // Pass event data we want to change
       dispatch(selectStyle(data));
     }}>
