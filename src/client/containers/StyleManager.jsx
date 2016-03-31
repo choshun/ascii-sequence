6957abbcbc8f3c6971d56daa5f7fd0a9d180b318
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-31 19:42:53 -0400

    style manager works!!

--- src/client/containers/StyleManager.jsx
+++ src/client/containers/StyleManager.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Layer from '../components/Layer.jsx';
-import Immutable from 'immutable';
 
-import { filter } from 'lodash';
+import { updateEventData, selectStyle } from '../actions';
+import { clone, findIndex } from 'lodash';
 import { connect } from 'react-redux';
 
-const StyleManager = ({ store, dispatch }) => (
+const StyleManager = ({ events, active, dispatch }) => (
   <section>
-    <textarea value={ store.styleManager.get('active').time + store.styleManager.get('active').data }></textarea>
+    <textarea value={ active.data } onChange={(event) => {
+      let index = _.findIndex(events.toArray(), {'key': active.key}),
+          activeStyle = _.clone(active),
+          styleValue = event.target.value;
+
+      activeStyle.data = styleValue;
+      dispatch(updateEventData({index, 'data': styleValue}));
+      dispatch(selectStyle(activeStyle));
+    }}></textarea>
   </section>
 );
 
 function mapStateToProps(store) {
   return {
-    store
+    events: store.events,
+    active: store.styleManager.get('active')
   };
 }
 
