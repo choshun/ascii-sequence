71a7382d62886dcf47b9610a711c5be1971789b1
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-30 20:51:56 -0400

    making default data work with new event adding

--- src/client/reducers/events.js
+++ src/client/reducers/events.js
@@ -3,37 +3,32 @@ import Immutable from 'immutable';
 let sqnc = [
   {
     'layer': 0,
-    'time': .75,
+    'time': 0.75,
     'callback': 'addStyle',
-    'data': '.layer2 { basdlob: of css};'
+    'data': '.layer2 { basdlob: of css};',
+    'key': 'event-00-75'
   },
   {
     'layer': 1,
-    'time': .55,
+    'time': 0.55,
     'callback': 'addStyle',
-    'data': '.layer2 { blasdob: of css};'
+    'data': '.layer2 { blasdob: of css};',
+    'key': 'event-10-55'
   },
   {
     'layer': 0,
     'time': 0,
     'callback': 'addStyle',
-    'data': '.layer1 { blobasd: of css};'
+    'data': '.layer1 { blobasd: of css};',
+    'key': 'event-00'
   }
 ];
 
 export default (state = Immutable.List(sqnc), action) => {
   switch(action.type) {
     case 'addEvent':
-      let eventState = {
-        'callback': 'addStyle',
-        'data': '.layer2 { blob: of css};'
-      };
-      eventState.layer = action.event.layer;
-      eventState.time = action.event.leftOffset;
-
-      //TODO: need to order by time, maybe for selecting by index
-
-      return state.push(eventState);
+      console.log('added action?', action);
+      return state.push(action.event);
     case 'deleteEvent':
       console.log('delete reducer?!?!', action);
       // return state.delete(action.index);
