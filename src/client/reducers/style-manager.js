d3b1da8235fe2cbcccbfc8abf17b12bc16b384ac
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-31 22:33:37 -0400

    cleanup

--- src/client/reducers/style-manager.js
+++ src/client/reducers/style-manager.js
@@ -2,13 +2,13 @@ import Immutable from 'immutable';
 
 let selectedState = {
   active: {
-    callback: "addStyle",
-    data: ".layer2 { blob: of css};",
-    key: "event-00-75",
+    callback: 'addStyle',
+    data: '.layer2 { blob: of css};',
+    key: 'event-00-75',
     layer: 0,
     time: 0.75
   }
-}
+};
 
 export default (state = Immutable.Map(selectedState), action) => {
   switch(action.type) {
