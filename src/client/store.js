d93d6ad1c8e0457e218dc92fd03095bcbe73866b
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-31 22:32:20 -0400

    adding transport to store

--- src/client/store.js
+++ src/client/store.js
@@ -2,11 +2,13 @@ import { combineReducers, createStore } from 'redux';
 import layers from './reducers/layers';
 import events from './reducers/events';
 import styleManager from './reducers/style-manager';
+import transport from './reducers/transport';
 
 let reducers = combineReducers({
   layers,
   events,
-  styleManager
+  styleManager,
+  transport
 });
 
 export default createStore(reducers);
