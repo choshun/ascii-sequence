320498fe7ef5b9c4a885cc2e74c131eaae28e015
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-03-31 21:13:24 -0400

    adding scene and scheduler to app.jsx

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -6,17 +6,25 @@ import { Provider } from 'react-redux';
 import store from './store';
 import Grid from './containers/Grid.jsx';
 import StyleManager from './containers/StyleManager.jsx';
+import Scheduler from './containers/Scheduler.jsx';
+import Scene from './containers/Scene.jsx';
 
 let app = document.getElementById('app');
 
-// have to make an app component, only one root :(
-// then in there style manager, grid, style, scene, etc
-
 render(
   <Provider store={store}>
-    <section className="container-wrapper">
+    <section className="containers-wrapper">
+    	{/* UI to edit events sequence. */}
       <Grid />
+
+      {/* UI to edit selected event. */}
       <StyleManager />
+
+      {/* Schedules events. */}
+      <Scheduler />
+
+      {/* Destination of events. */}
+      <Scene />
     </section>
   </Provider>,
   app
