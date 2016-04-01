0bd4e7b830ad4192a5bab5aca5fcda0c9b61afc4
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-01 18:03:15 -0400

    transport time to 5

--- src/client/reducers/transport.js
+++ src/client/reducers/transport.js
@@ -4,7 +4,7 @@ let initialTransport = {
   'play': true, // Should be playing.
   'start': 0, // Start of loop play as fraction of total time.
   'end': 1,	// End of loop play as fraction of total time.
-  'time': 30 // Total loop time in seconds.
+  'time': 5 // Total loop time in seconds.
 };
 
 export default (state = Immutable.Map(initialTransport), action) => {
