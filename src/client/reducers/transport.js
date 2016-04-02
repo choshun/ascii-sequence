c7d2285db7983acf0bfab090b9477fd10be7cc58
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-01 22:56:21 -0400

    adding context to transport

--- src/client/reducers/transport.js
+++ src/client/reducers/transport.js
@@ -1,10 +1,15 @@
 import Immutable from 'immutable';
+import Utils from '../utils/utils.js';
+
+const utils = new Utils();
+utils.startContext();
 
 let initialTransport = {
   'play': true, // Should be playing.
   'start': 0, // Start of loop play as fraction of total time.
   'end': 1,	// End of loop play as fraction of total time.
-  'time': 5 // Total loop time in seconds.
+  'time': 5, // Total loop time in seconds.
+  'context': utils.getContext() // Audio context that keeps time.
 };
 
 export default (state = Immutable.Map(initialTransport), action) => {
