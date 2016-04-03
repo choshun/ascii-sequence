0ec27a92985e3f7caad34fce4aabc44dd0d98005
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-02 20:34:50 -0400

    adding css destination

---
+++ src/client/destinations/css.js
@@ -0,0 +1,10 @@
+// Add event at correct time
+// Clear at new loop
+// Concept of persistent css (per layer or grid/scene(bg)), maybe initial state?
+class Css {
+  addStyle(event) {
+    console.log('event', event.data, event.key);
+  }
+}
+
+export default Css;
