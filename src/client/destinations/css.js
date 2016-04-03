25febdb3d9abbb0c7951f8f307fab6695167f51a
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-02 22:39:54 -0400

    wow, how did that get thrown out?

--- src/client/destinations/css.js
+++ src/client/destinations/css.js
@@ -1,9 +1,23 @@
-// Add event at correct time
+// Add trainsition delay from schedule
 // Clear at new loop
 // Concept of persistent css (per layer or grid/scene(bg)), maybe initial state?
 class Css {
+  constructor() {
+    this.styleBlock = document.createElement('style');
+  }
+
+  init() {
+    document.head.appendChild(this.styleBlock);
+  }
+
+  createClass(event) {
+    // TODO: make scene a constant
+    return `.scene .${event.key} {\n${event.data}\n}`;
+  }
+
   addStyle(event) {
-    console.log('event', event.data, event.key);
+    // TODO: clear out old stuff in style tag if possible (new loop?)
+    this.styleBlock.innerHTML = this.styleBlock.innerHTML + `${this.createClass(event)}\n`;
   }
 }
 
