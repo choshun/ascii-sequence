f362d550bace1dc013caaa06914c9b39cc57e259
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-04 15:40:34 -0400

    create event put into Grid

--- src/client/components/layer.jsx
+++ src/client/components/layer.jsx
@@ -2,38 +2,13 @@ require('./layer.scss');
 
 import React from 'react';
 import Event from './Event.jsx';
-
-import { addEvent, selectStyle } from '../actions';
 import { connect } from 'react-redux';
 
-function createEvent(leftOffset, layer) {
-  return Object.assign({}, {
-    'layer': layer,
-    'time': leftOffset,
-    'callback': 'addStyle',
-    'data': 'new data: of css;\nleft: 50px',
-    'key': createKey(leftOffset, layer)
-  });
-}
-
-function createKey(leftOffset, layer) {
-  return `event-${layer}${leftOffset}`.replace(/\./g, '-');
-}
-
-const Layer = ({ data, css, layer, dispatch }) => (
-  <li className={'layer'} onClick={(event) => {
-      if (event.target.classList.contains('layer')) {
-        let leftOffset = event.pageX / window.innerWidth,
-            newEvent = createEvent(leftOffset, layer);
-
-        dispatch(addEvent(newEvent));
-        dispatch(selectStyle(newEvent));
-      }
-    }}>
-
+const Layer = ({ layerData, layer, onClick, dispatch }) => (
+  <li className={'layer'} onClick={(event) => { onClick(event, layer, dispatch) }}>
     <ul className={'events'}>
-      { data.map((event, index) => {
-        return <Event key={event.key} data={event} css={css[event.key]} />;
+      { layerData.map((event, index) => {
+        return <Event key={event.key} eventData={event} />;
       }) }
     </ul>
   </li>
