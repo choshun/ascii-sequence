42812bd49a09d430f1387a7ce5c3ce18cf1599ed
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-04 17:19:58 -0400

    layer refactor, using Component

--- src/client/components/layer.jsx
+++ src/client/components/layer.jsx
@@ -1,23 +1,25 @@
 require('./layer.scss');
 
-import React from 'react';
+import React, { Component } from 'react';
 import Event from './Event.jsx';
 import { connect } from 'react-redux';
 
-const Layer = ({ layerData, layer, onClick, dispatch }) => (
-  <li className={'layer'} onClick={(event) => { onClick(event, layer, dispatch) }}>
-    <ul className={'events'}>
-      { layerData.map((event, index) => {
-        return <Event key={event.key} eventData={event} />;
-      }) }
-    </ul>
-  </li>
-);
+class Layer extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-function mapStateToProps(store) {
-  return {
-    store
+  render() {
+    return (
+      <li className={'layer'} onClick={(event) => { this.props.onClick(event, this.props.layer, this.props.dispatch) }}>
+        <ul className={'events'}>
+          { this.props.layerData.map((event, index) => {
+            return <Event key={event.key} eventData={event} styleManager={this.props.styleManager} />;
+          }) }
+        </ul>
+      </li>
+    );
   }
 }
 
-export default connect(mapStateToProps)(Layer);
+export default connect()(Layer);
