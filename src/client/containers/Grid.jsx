43d2a9cd687f2853ca26ca2d54788715fe7bfd0a
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-04 21:06:06 -0400

    Grid refactor to use Component

--- src/client/containers/Grid.jsx
+++ src/client/containers/Grid.jsx
@@ -2,76 +2,92 @@
  * This is responsible for the event UI.
  * You can add, select, and remove events.
 **/
-import React from 'react';
+import React, { Component } from 'react';
 import Layer from '../components/Layer.jsx';
-import { uniq, map, each, filter } from 'lodash';
+import { clone, uniq, map, each, filter } from 'lodash';
 import { addEvent, selectStyle } from '../actions';
 import { connect } from 'react-redux';
 
-let styleBlock = document.createElement('style'),
-    initialized = false;
+class Grid extends Component {
+  constructor(props) {
+    super(props);
 
-document.head.appendChild(styleBlock);
-
-// TODO: get from events, or from a constant
-function createEvent(leftOffset, layer) {
-  return Object.assign({}, {
-    'layer': layer,
-    'time': leftOffset,
-    'callback': 'addStyle',
-    'data': 'new data: of css;\nleft: 50px',
-    'key': createKey(leftOffset, layer)
-  });
-}
-
-function addGridStyleToHead(className, css) {
-  styleBlock.innerHTML = styleBlock.innerHTML + `.grid .${className} ${css}\n`;
-}
-
-function createKey(leftOffset, layer) {
-  return `event-${layer}${leftOffset}`.replace(/\./g, '-');
-}
-
-function createCSS(property, value) {
-  return `{ ${property}: ${value} }`;
-}
-
-function createPosition(time) {
-  console.log('time', time);
-  return time * 100 + '%';
-}
-
-function layerClick(event, layer, dispatch) {
-  if (event.target.classList.contains('layer')) {
-    let leftOffset = event.pageX / window.innerWidth,
-        newEvent = createEvent(leftOffset, layer),
-        left = createPosition(newEvent.time),
-        css = createCSS('left', left);
-
-    dispatch(addEvent(newEvent));
-    dispatch(selectStyle(newEvent));
-    addGridStyleToHead(newEvent.key, css);
+    this.styleBlock = document.createElement('style');
   }
-}
 
-function initGridCSS(events) {
-  if (!initialized) {
+  componentDidMount() {
+    this.initGridCSS(this.props.store.events);
+    document.head.appendChild(this.styleBlock);
+  }
+
+  // TODO: use events store, or make a constant
+  createEvent(leftOffset, layer) {
+    return {
+      'layer': layer,
+      'time': leftOffset,
+      'callback': 'addStyle',
+      'data': 'new data: of css;\nleft: 50px',
+      'key': this.createKey(leftOffset, layer)
+    };
+  }
+
+  addGridStyleToHead(className, css) {
+    this.styleBlock.innerHTML = this.styleBlock.innerHTML + `.grid .${className} ${css}\n`;
+  }
+
+  createKey(leftOffset, layer) {
+    return `event-${layer}${leftOffset}`.replace(/\./g, '-');
+  }
+
+  createPosition(time) {
+    return time * 100 + '%';
+  }
+
+  createCSS(property, value) {
+    return `{ ${property}: ${value} }`;
+  }
+
+  initGridCSS(events) {
     _.each(events.toObject(), (item, index) => {
-        let left = createPosition(item.time),
-            key = item.key,
-            css = createCSS('left', left);
+      let left = this.createPosition(item.time),
+          key = item.key,
+          css = this.createCSS('left', left);
 
-        addGridStyleToHead(key, css);
-
-        if (parseInt(index) === (events.size - 1)) {
-          initialized = true;
-        }
+      this.addGridStyleToHead(key, css);
     });
+  }
+
+  layerClick(event, layer, dispatch) {
+    if (event.target.classList.contains('layer')) {
+      let leftOffset = event.pageX / window.innerWidth,
+          newEvent = this.createEvent(leftOffset, layer),
+          left = this.createPosition(newEvent.time),
+          css = this.createCSS('left', left);
+
+      dispatch(addEvent(newEvent));
+      dispatch(selectStyle(newEvent));
+      this.addGridStyleToHead(newEvent.key, css);
+    }
+  }
+
+  render() {
+    return (
+      <section className={'grid'}>
+        Grid!!
+
+        <ul className={'layers'}>
+          {this.props.layers.map((layer, index) => {
+            return <Layer key={index} layerData={layer} layer={index} onClick={this.layerClick.bind(this)} styleManager={this.props.store.styleManager} />;
+          })}
+        </ul>
+
+      </section>
+    );
   }
 }
 
 function createLayersFromEvents(layers, events) {
-  let eventsObject = events.toObject(),
+  let eventsObject = _.clone(events).toObject(),
       uniqueLayers = _.uniq(_.map(eventsObject, 'layer')),
       layersAndEvents = [];
 
@@ -83,24 +99,10 @@ function createLayersFromEvents(layers, events) {
   return layersAndEvents;
 }
 
-const Grid = ({ store, layers, gridCSS }) => (
-  <section className={'grid'}>
-    Grid!!
-
-    <ul className={'layers'}>
-      {layers.map((layer, index) => {
-        return <Layer key={index} layerData={layer} layer={index} onClick={layerClick} styleManager={store.styleManager} />;
-      })}
-    </ul>
-
-  </section>
-);
-
 function mapStateToProps(store) {
   return {
     store,
-    'layers': createLayersFromEvents(store.layers, store.events),
-    'gridCSS': initGridCSS(store.events)
+    'layers': createLayersFromEvents(store.layers, store.events)
   };
 }
 
