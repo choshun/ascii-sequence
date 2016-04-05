6b9056cc68414d39902f29fcee4979299114543b
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-04 22:02:48 -0400

    refactoring Transport to us Component

--- src/client/containers/Transport.jsx
+++ src/client/containers/Transport.jsx
@@ -1,27 +1,31 @@
 /** 
  * Controls time! :0
 **/
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class TransportUtils {
-  mapStateToProps(store) {
-    return {
-      store
-    };
+class Transport extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className={'scene'}>
+        Transport!!!
+        // TODO: make into component, set transport.play to !play
+        // TODO: make sure context doesn't reset very time transport is changed,
+        // if it does, see if it matters (maybe it doesn't?)
+        <button className={'play'}>play button yo</button>
+      </section>
+    );
   }
 }
 
-const transportUtils = new TransportUtils();
+function mapStateToProps(store) {
+  return {
+    store
+  };
+}
 
-const Transport = ({ layers, gridCSS }) => (
-  <section className={'scene'}>
-    Transport!!!
-    // TODO: make into component, set transport.play to !play
-    // TODO: make sure context doesn't reset very time transport is changed,
-    // if it does, see if it matters (maybe it doesn't?)
-    <button className={'play'}>play</button>
-  </section>
-);
-
-export default connect(transportUtils.mapStateToProps)(Transport);
+export default connect(Transport.mapStateToProps)(Transport);
