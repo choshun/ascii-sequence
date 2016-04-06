d5263569e96050a65190fce94f0b93e43df1d60d
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-04-05 21:12:48 -0400

    more events for testing

--- src/client/reducers/events.js
+++ src/client/reducers/events.js
@@ -2,23 +2,42 @@ import Immutable from 'immutable';
 
 let sqnc = [
   {
+    'layer': 1,
+    'time': 0.25,
+    'callback': 'addStyle',
+    'class': '.layer-1',
+    'data': 'basdlob: of css;\nleft: 100px',
+    'key': 'event-10-25'
+  },
+  {
     'layer': 0,
     'time': 0.75,
     'callback': 'addStyle',
-    'data': 'blob: of css;\nleft: 20px',
+    'class': '.layer-0',
+    'data': 'blob: of css;\nleft: 200px',
     'key': 'event-00-75'
+  },
+  {
+    'layer': 0,
+    'time': 0.9,
+    'callback': 'addStyle',
+    'class': '.layer-0',
+    'data': 'blob: of css;\nleft: 500px',
+    'key': 'event-00-9'
   },
   {
     'layer': 1,
     'time': 0.55,
     'callback': 'addStyle',
-    'data': 'basdlob: of css;\nleft: 30px',
+    'class': '.layer-1',
+    'data': 'basdlob: of css;\nleft: 300px',
     'key': 'event-10-55'
   },
   {
     'layer': 0,
     'time': 0,
     'callback': 'addStyle',
+    'class': '.layer-0',
     'data': 'assdlob: of css;\nleft: 50px',
     'key': 'event-00'
   }
