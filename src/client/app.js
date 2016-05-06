78e01a75619e3ae95006ea24932efb75999f819d
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-05-06 16:56:58 -0400

    Moving app.js to vue

---
+++ src/client/app.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Revue from 'revue';
+import {createStore} from 'redux';
+import Transport from './containers/Transport.vue';
+
+new Vue({
+  el: '#app',
+  components: {
+    Transport
+  }
+});
+
