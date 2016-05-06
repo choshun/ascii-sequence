da87301584757647c3f80d6f79a1d907a22336ee
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-05-06 16:57:16 -0400

    Transport as first POC component.

---
+++ src/client/containers/Transport.vue
@@ -0,0 +1,29 @@
+<style scoped lang="sass">
+	.scene {
+	  color: red;
+
+	  .nested {
+	  	color: green;
+	  }
+	}
+</style>
+
+<template>
+	<section class="scene">
+    Transport!!?
+    <span class="nested">{{ test }}</span>
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      test: `I'm a test yo!`
+      
+    }
+  }
+}
+
+</script>
