3748e28921e63691855888c1348c49ca91de4e27
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-05-16 11:59:21 -0400

    adding transport time cue

---
+++ src/client/components/TransportTime.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="transport-time">
+    <input type="number" value={ this.props.transport.get('time') } onChange={ this.changeTime } /> oh hai
+  </section>
+</template>
+
+<script>
+  export default {
+    methods: {
+      changeTime: function(event) {
+        this.props.dispatch(setTime(event.target.value));
+      }
+    }
+  }
+</script>
