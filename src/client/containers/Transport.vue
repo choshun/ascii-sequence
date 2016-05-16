17421687eab079cf6d321e2c9a330e13ddd396df
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-05-16 10:43:57 -0400

    redux store wired up

--- src/client/containers/Transport.vue
+++ src/client/containers/Transport.vue
@@ -11,18 +11,23 @@
 <template>
 	<section class="scene">
     Transport!!?
-    <span class="nested">{{ test }}</span>
+    <span class="nested" v-on:click="log">{{ test }}</span>
   </section>
 </template>
 
 <script>
+  import store from '../store';
 
-export default {
-  data() {
-    return {
-      test: `I'm a test yo!!?`
+  export default {
+    data() {
+      return {
+        transport: this.$select('transport')
+      }
+    },
+    methods: {
+      log: function() {
+        console.log('transport?', this.transport.toObject(), 'events?', this.events.toArray(), this.events.toObject(), this.events.toJS()); // need to loop through, since it's a collection of maps. then do .toArrray()
+      }
     }
   }
-}
-
 </script>
--- src/client/store.js
+++ src/client/store.js
@@ -1,14 +1,20 @@
+import Vue from 'vue';
+import Revue from 'revue';
 import { combineReducers, createStore } from 'redux';
+import actions from './actions';
 import layers from './reducers/layers';
 import events from './reducers/events';
 import styleManager from './reducers/style-manager';
 import transport from './reducers/transport';
 
-let reducers = combineReducers({
+const reducers = combineReducers({
   layers,
   events,
   styleManager,
   transport
 });
 
-export default createStore(reducers);
+const reduxStore = createStore(reducers);
+const store = new Revue(Vue, reduxStore, actions);
+
+export default store;
