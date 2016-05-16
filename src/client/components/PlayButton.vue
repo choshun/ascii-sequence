5e30382007576d46cfc23c519268e4b581c705f3
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-05-16 11:59:08 -0400

    adding play button cue

---
+++ src/client/components/PlayButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button className={ 'play-button' } v-on:click="togglePlay">
+    play button yo
+  </button>
+</template>
+
+<script>
+  export default {
+    methods: {
+      togglePlay: function() {
+        var playing = !this.props.transport.get('play');
+        this.props.dispatch(setPlay(playing));
+      }
+    }
+  }
+</script>
