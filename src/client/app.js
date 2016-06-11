33b9ca1aec631afd4b74ab32776516164f04fcb0
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-11 17:33:34 -0400

    adding grid to app

--- src/client/app.js
+++ src/client/app.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
-import Transport from './containers/Transport.vue';
+import Transport from './components/Transport.vue';
+import Grid from './components/Grid.vue';
 
 new Vue({
   el: '#app',
   components: {
-    Transport
+    Transport,
+    Grid
   }
 });
 
