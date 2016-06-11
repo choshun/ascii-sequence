b7d7c083ed511e1abd975b83fa58757d6fd63fd9
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-11 17:34:18 -0400

    adding layer.vu

---
+++ src/client/components/Layer.vue
@@ -0,0 +1,58 @@
+<style scoped lang="sass">
+	.layer {
+    border-bottom: 1px solid #000;
+    background-color: rgba(193, 66, 66, .3);
+    height: 4em;
+    opacity: .7;
+    position: relative;
+  }
+</style>
+
+<template>
+  <li class="layer" @click="init()" >
+    <ul>
+      {{ layerCSS }}
+      <event v-for="event in sequence[layer]" track-by="$index" :layer="layer" :event="event" ></event>
+    </ul>
+  </li>
+</template>
+
+<script>
+  import store from '../vuex/store';
+  import Event from './Event.vue';
+  import { getEventsByLayer } from '../vuex/getters';
+  import { clone, uniq, map, each, filter } from 'lodash';
+
+  class Layer {
+    // TODO: put in getter
+    createLayersFromEvents(sequence) {
+      let eventsObject = _.clone(sequence),
+      uniqueLayers = _.uniq(_.map(eventsObject, 'layer')),
+      layersAndEvents = [];
+
+      // Creates new object of Layers with events for ui.
+      _.each(uniqueLayers, (item, index) => {
+        layersAndEvents.push(_.filter(eventsObject, {layer: index}));
+      });
+
+      return layersAndEvents;
+    }
+  }
+
+  var layer = new Layer();
+
+  export default {
+    store,
+    props: ['layer', 'sequence'],
+    vuex: {
+      getters: {
+        sequence: store => {
+          return layer.createLayersFromEvents(store.sequence);
+        }
+      }
+    },
+    components: {
+      Event
+    }
+  }
+</script>
