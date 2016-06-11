5968d95ae08e311a0edd78e74f687773ee1b8a8e
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-11 17:35:52 -0400

    updating store so it has default sequence

--- src/client/vuex/store.js
+++ src/client/vuex/store.js
@@ -1,25 +1,76 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+// TODO: Should prolly put this in transport.vue
+import Utils from '../utils/utils.js';
+const utils = new Utils();
+utils.createContext();
+
 Vue.use(Vuex);
 
 const state = {
-  // When the app starts, count is set to 0
-  count: 0,
   transport: {
   	playing: true,
   	time: 5
-  }
+  },
+  sequence: [
+	  {
+	    'layer': 1,
+	    'time': 0.25,
+	    'callback': 'addStyle',
+	    'class': '.layer-1',
+	    'data': 'basdlob: of css;\nleft: 100px',
+	    'key': 'event-10-25'
+	  },
+	  {
+	    'layer': 2,
+	    'time': 0.75,
+	    'callback': 'addStyle',
+	    'class': '.layer-0',
+	    'data': 'blob: of css;\nleft: 200px',
+	    'key': 'event-00-75'
+	  },
+	  {
+	    'layer': 1,
+	    'time': 0.55,
+	    'callback': 'addStyle',
+	    'class': '.layer-1',
+	    'data': 'basdlob: of css;\nleft: 300px',
+	    'key': 'event-10-55'
+	  },
+	  {
+	    'layer': 0,
+	    'time': 0,
+	    'callback': 'addStyle',
+	    'class': '.layer-0',
+	    'data': 'assdlob: of css;\nleft: 50px',
+	    'key': 'event-00'
+	  }
+	],
+  layers: [
+    {
+      'index': 0,
+      'element': ':'
+    },
+    {
+      'index': 1,
+      'element': ')'
+    },
+    {
+      'index': 2,
+      'element': '>'
+    }
+  ]
 }
 
 const mutations = {
   // A mutation receives the current state as the first argument
   // You can make any modifications you want inside this function
-  INCREMENT (state, amount) {
-    state.count = state.count + amount;
-  },
   TOGGLEPLAY (state) {
     state.transport.playing = !state.transport.playing;
+  },
+  UPDATE_TIME (state, time) {
+  	state.transport.time = time;
   }
 }
 
