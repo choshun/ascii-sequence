ca2566863dacc81e90471fb9e952860d66daa2ed
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-11 17:34:06 -0400

    adding grid.vu

---
+++ src/client/components/Grid.vue
@@ -0,0 +1,32 @@
+<style scoped lang="sass">
+	.grid {
+		position: relative;
+	}
+</style>
+
+<template>
+  <section class="grid">
+    Grid!!
+    {{ gridCSS }}
+    <ul class="layers">
+      <layer v-for="layer in layers" :layer="$index"></layer>
+    </ul>
+  </section>
+</template>
+
+<script>
+  import store from '../vuex/store';
+  import Layer from './Layer.vue';
+
+  export default {
+    store,
+    vuex: {
+      getters: {
+        layers: store => store.layers,
+      }
+    },
+    components: {
+      Layer
+    }
+  }
+</script>
