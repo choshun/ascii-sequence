c6afb56df76cd7b759f2b39dd888badeb4955cd1
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-12 11:32:41 -0400

    adding styleManager

--- src/client/app.js
+++ src/client/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Transport from './components/Transport.vue';
 import Grid from './components/Grid.vue';
+import StyleManager from './components/StyleManager.vue';
 
 new Vue({
   el: '#app',
   components: {
     Transport,
-    Grid
+    Grid,
+    StyleManager
   }
 });
 
