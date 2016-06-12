9682b2d9374932fc88e3bed9a55024cad509ec4f
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-12 19:53:18 -0400

    scene added!!!! works :)

--- src/client/components/Scene.vue
+++ src/client/components/Scene.vue
@@ -1,13 +1,30 @@
 <style scoped lang="sass">
+  $ease: cubic-bezier(.23, 1, .32, 1);
+
   .scene{
     height: 100%;
     position: fixed;
+
+    .scene-item {
+      font-size: 4em;
+      position: absolute;
+      transition: all .5s $ease;
+    }
+    
   }
 </style>
 
 <template>
 	<section class="scene">
     Scene!
+
+    <ul>
+      <li v-for="layer in layers">
+        <figure class="layer-{{$index}} scene-item" track-by="$index">
+          {{ layer.element }}
+        </figure>
+      </li>
+    </ul>
   </section>
 </template>
 
