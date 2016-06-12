8cef29724845e750ef03ecbe7043d9ffd9863825
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-12 19:29:54 -0400

    adding scene.vue!

---
+++ src/client/components/Scene.vue
@@ -0,0 +1,25 @@
+<style scoped lang="sass">
+  .scene{
+    height: 100%;
+    position: fixed;
+  }
+</style>
+
+<template>
+	<section class="scene">
+    Scene!
+  </section>
+</template>
+
+<script>
+  import store from '../vuex/store';
+
+  export default {
+    store,
+    vuex: {
+      getters: {
+        layers: store => store.layers
+      }
+    }
+  }
+</script>
