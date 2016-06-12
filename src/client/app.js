d09a4d0f094b1ed43e5a694d0afab6102563c935
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-12 14:08:19 -0400

    adding references to scheduler

--- src/client/app.js
+++ src/client/app.js
@@ -2,13 +2,15 @@ import Vue from 'vue';
 import Transport from './components/Transport.vue';
 import Grid from './components/Grid.vue';
 import StyleManager from './components/StyleManager.vue';
+import Scheduler from './components/Scheduler.vue';
 
 new Vue({
   el: '#app',
   components: {
     Transport,
     Grid,
-    StyleManager
+    StyleManager,
+    Scheduler
   }
 });
 
