1725a58e4e517613d28fc499a8b8844273f26ad0
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-12 11:34:12 -0400

    adding style manager component

---
+++ src/client/components/StyleManager.vue
@@ -0,0 +1,29 @@
+<style>
+  
+</style>
+
+<template>
+	<section className={'style-manager'}>
+    <textarea :value="getActiveStyle" @input="onStyleChange"></textarea>
+  </section>
+</template>
+
+<script>
+  import store from '../vuex/store';
+
+  export default {
+    store,
+    vuex: {
+      getters: {
+        getActiveStyle: store => store.styleManager.active[0].data
+      },
+      actions: {
+        onStyleChange: ({ dispatch }, event) => {
+          console.log('boop, style change motherfucker');
+
+          // update selected event with input value
+        }
+      }
+    }
+  }
+</script>
