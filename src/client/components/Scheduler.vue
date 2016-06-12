3942170f353abf2077939c1d9d040a8dff3eb6b0
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-12 14:08:31 -0400

    added scheduler!

---
+++ src/client/components/Scheduler.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="scheduler">
+    Scheduler!!! :D
+  </section>
+</template>
+
+<script>
+  import store from '../vuex/store';
+
+  export default {
+    store,
+    vuex: {
+      getters: {
+        layers: store => store.layers
+      }
+    }
+  }
+</script>
