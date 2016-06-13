944dd662c2c7498a10e71c37a3b21ef2c4b23c46
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-13 14:49:00 -0400

    cleaning up web pack to only work with vues (sass loader wasn't working with cue loader afaik)

--- config/webpack.js
+++ config/webpack.js
@@ -1,5 +1,4 @@
 var path = require('path');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
@@ -14,29 +13,12 @@ module.exports = {
       {
         test: /(\.jsx?|\.js?)/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      },
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('css!sass')
+        loader: 'babel'
       },
       {
         test: /\.vue$/,
         loader: 'vue'
       }
     ]
-  },
-
-  sassLoader: {
-    includePaths: [path.resolve(__dirname, '../src/client/sass')]
-  },
-
-  plugins: [
-    new ExtractTextPlugin('style.css', {
-      allChunks: true
-    })
-  ]
+  }
 };
