9c219e2d95ef2390ab92c054f334d6418f0cd2c2
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-06-15 21:41:31 -0400

    transport start and duration for sublets

--- src/client/components/Transport.vue
+++ src/client/components/Transport.vue
@@ -10,16 +10,16 @@
 
 <template>
 	<section class="transport">
-    Transport!!?
-
     <section class="play-button">
       {{ getIsPlaying }}
       <button @click="togglePlay">Play Button</button>
     </section>
 
-    {{ getTime }}
+    duration {{ getDuration }}
     <section class="transport-getTime">
       <input :value="getTime" type="number" @input="updateTime" /> loop time
+      <input :value="getStart" type="number" @input="updateStart" /> loop start
+      <input :value="getDuration" type="number" step="any" @input="updateDuration" /> loop end
     </section>
 
   </section>
@@ -33,11 +33,15 @@
     vuex: {
       getters: {
         getIsPlaying: state => state.transport.playing,
-        getTime: store => store.transport.time
+        getTime: store => store.transport.time,
+        getStart: store => store.transport.start,
+        getDuration: store => store.transport.duration
       },
       actions: {
         togglePlay: ({ dispatch }) => dispatch('TOGGLEPLAY'),
-        updateTime: ({ dispatch }, event) => dispatch('UPDATE_TIME', event.target.value)
+        updateTime: ({ dispatch }, event) => dispatch('UPDATE_TIME', event.target.value),
+        updateStart: ({ dispatch }, event) => dispatch('UPDATE_START', event.target.value),
+        updateDuration: ({ dispatch }, event) => dispatch('UPDATE_DURATION', event.target.value)
       }
     }
   }
