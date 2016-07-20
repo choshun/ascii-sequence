e81dfd6e4f5c868c94314105a00282f3a0ffa93b
csnyder@hugeinc.com <csnyder@H6293.ad.hugeinc.com>
2016-07-20 16:33:10 -0400

    FINALY GROCKED. scheduler, the vuex way.

--- src/client/components/Scheduler.vue
+++ src/client/components/Scheduler.vue
@@ -1,131 +1,143 @@
-<template>
-  <section class="scheduler">
-    {{ getSortedSequence(schedule) }}
-  </section>
-</template>
-
 <script>
   import store from '../vuex/store';
   import Css from '../destinations/css.js';
   import ContextUtils from '../utils/context-utils.js';
   import { clone, cloneDeep, findIndex, slice, forEach } from 'lodash';
 
+  // TODO: Put in vue component.
   const contextUtils = new ContextUtils();
 
-  /**
-   * @class Scheduler
-   * A sequencer using audio context for timing.
-   */
-  class Scheduler {
-    /**
-     * @constructor Scheduler
-     * @param {Object} cube cube object to schedule
-     */
-    constructor() {
-      /**
-       * Destination callback class.
-       * @type {Class}
-       */
-      this.destination = new Css();
+  export default {
+    store,
+    data () {
+      return {
+
+        /**
+         * Destination callback.
+         * @type {Function}
+         */
+        destination: () => new Css(),
+
+        /**
+         * Amount to poll for changes in store.sequence (in ms).
+         * @type {Number}
+         */
+        lookahead: 30,
+
+        /**
+         * Amount to look ahead from currentTime 
+         * (in percentage of 1 - the total sequence time).
+         * @type {Number}
+         */
+        scheduleAheadTime: 0.01,
+
+        /**
+         * Current event scheduled.
+         * @type {Number}
+         */
+        index: 0,
+
+        /**
+         * Length of scheduled sequence.
+         * @type {Number}
+         */
+        length: 0,
+
+        /**
+         * Flag so the same event isn't fired twice in a row.
+         * @type {Number}
+         */
+        firedEventTime: 0,
+
+        /**
+         * Reference to the scheduler timeout, so we can
+         * make sure it's cleared if it's reinstantiated by mistake.
+         * @type {Function}
+         */
+        scheduleTimeout: undefined
+      }
+    },
+    vuex: {
+      getters: {
+
+        /**
+         * Ordered sequence from store.
+         * @type {Array}
+         */
+        sequence: store => {
+          return _.cloneDeep(store.sequence.sort((a, b) => {
+            return a.time - b.time;
+          }));
+        },
+
+        /**
+         * Transport from store.
+         * @type {Object}
+         */
+        transport: store => store.transport
+      }
+    },
+    methods: {
 
       /**
-       * Interval to try and refire schedule.
-       * @type {Number} In ms.
+       * Schedules events and fires the destination callback.
        */
-      this.lookahead = 30;
+      schedule () {
+        if (this.transport.playing) {
+          let time = contextUtils.getTranslatedContext(this.transport);
 
-      /**
-       * Time to look ahead in sequence to schedule.
-       * @type {Number} Percent of total sequence.
-       */
-      this.scheduleAheadTime = 0.01;
+          this.index = this.getNextIndex(this.sequence, this.transport, time);
 
-      /**
-       * Index of scheduled events.
-       * @type {Number}
-       */
-      this.index = 0;
+          if (this.index !== -1) {
+            let nextEvent = this.sequence[this.index];
+            let eventTime = nextEvent.time;
 
-      this.length = 0;
+            // The infamous article explaining the following:
+            // http://www.html5rocks.com/en/tutorials/audio/scheduling/
+            if (eventTime !== this.firedEventTime && eventTime < time + this.scheduleAheadTime) {
+              this.length = this.sequence.length;
 
-      this.firedEventTime = 0;
-    }
+              let reset = (this.index === this.length);
+              // Fires event callback.
+              this.destination()[nextEvent.callback](nextEvent, reset);
 
-    /**
-     * Scheduler for sequence of events. Fires a destination
-     * based on event time in store "events".
-     * It's a scheduler, so there's a lookahead and all that other good stuff:
-     * http://www.html5rocks.com/en/tutorials/audio/scheduling/
-     */
-
-    schedule(sequence, transport) {
-      if (transport.playing) {
-        let time = contextUtils.getTranslatedContext(transport);
-
-        this.index = schedulerClass.getNextIndex(sequence, transport, time);
-
-        if (this.index !== -1) {
-          let nextEvent = sequence[this.index];
-          let eventTime = nextEvent.time;
-
-          if (eventTime !== this.firedEventTime && eventTime < time + this.scheduleAheadTime) {
-            this.length = sequence.length;
-            let reset = (this.index === this.length);
-            // Fires event callback.
-            this.destination[nextEvent.callback](nextEvent, reset);
-
-            this.firedEventTime = eventTime;
+              this.firedEventTime = eventTime;
+            }
           }
         }
 
+        this.cycle();
+      },
+
+      /**
+       * Never not polling.
+       */
+      cycle () {
         if (this.scheduleTimeout) {
           clearTimeout(this.scheduleTimeout);
         }
 
         this.scheduleTimeout = window.setTimeout(() => {
-          this.schedule(sequence, transport);
+          this.schedule();
         }, this.lookahead);
-      }
-    }
+      },
 
-    getNextIndex(sequence, transport, time) {
-
-      // console.log(sequence);
-      return _.findIndex(sequence, (event) => {
-        return event.time + this.scheduleAheadTime > time;
-      });
-    }
-  }
-
-  // TODO: maybe put constructor in data
-  const schedulerClass = new Scheduler();
-
-  export default {
-    store,
-    vuex: {
-      getters: {
-        schedule: store => store.schedule
-        // For whatever reason using lodash made store.sequence sync horribly when I used _.sort
-        // schedule: (store) => {
-        //   console.log('fuck', this, store);
-        //   schedulerClass.schedule(this.getSortedSequence(_.cloneDeep(store.sequence)), store.transport);
-        // }
-
+      /**
+       * Finds the next event based on transport time.
+       * @param {Array} sequence Sequence shared in store.
+       * @param {Object} transport Transport object in store.
+       * @param {Number} time Current time.
+       * @return {Number} Index of next event. 
+       */
+      getNextIndex (sequence, transport, time) {
+        return _.findIndex(sequence, (event) => {
+          return event.time + this.scheduleAheadTime > time;
+        });
       }
     },
-    methods: {
-      getSortedSequence (schedule) {
-        console.log('?!?!?!?', this, schedule);
-        // return sequence.sort((a, b) => {
-        //   return a.time - b.time;
-        // });
-      },
-      test () {
-        console.log('tesst');
-      }
-    }, 
     created () {
-      console.log('?', this, this.schedule);
+      // Kick off the polling.
+      this.schedule(); 
     }
   }
 </script>
