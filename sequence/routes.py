4a4c4d314e3c149fd2a5b969c94cd77d866beb9a
Choshun Snyder <choshun.snyder@gmail.com>
2017-02-08 19:32:21 -0500

    Checking config.

--- sequence/routes.py
+++ sequence/routes.py
@@ -3,12 +3,12 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def home():
   return render_template('app.html')
- 
+
 if __name__ == '__main__':
   app.run()
 
-print(os.environ['APP_SETTINGS'])
