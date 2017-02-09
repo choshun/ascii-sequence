d1578909244387d499faa181529e9c40e9c5a270
Choshun Snyder <choshun.snyder@gmail.com>
2017-02-08 19:20:08 -0500

    app test for environment.

--- sequence/routes.py
+++ sequence/routes.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask, render_template
  
-app = Flask(__name__)      
- 
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+
 @app.route('/')
 def home():
   return render_template('app.html')
  
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run()
+
+print(os.environ['APP_SETTINGS'])
